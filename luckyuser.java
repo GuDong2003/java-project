38f858b8a50d97bd451c7a5f61ab988eb4384bf1
咕咚 <1735590698@qq.com>
2023-03-16 22:49:08 +0800

    加了点注释

--- luckyuser.java
+++ luckyuser.java
@@ -16,9 +16,9 @@ public class luckyuser {
         System.out.println("请输入客户四位号码：");
         num = sc.nextInt();
         ge = num % 10;// 个位数字
-        shi = num / 10 % 10;
-        bai = num / 100 % 10;
-        qian = num / 1000 % 10;
+        shi = num / 10 % 10;// 十位数字
+        bai = num / 100 % 10;// 百位数字
+        qian = num / 1000 % 10;// 千位数字
         total = ge + shi + bai + qian;
         if (ge == 8 || shi == 8 || bai == 8 || qian == 8 || 8 % total != 0)
             System.out.println("该用户不是幸运客户！");
